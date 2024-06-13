21f220ca7a774c192b39798d7f059a36d3ff18dd
Siergej Sobolewski <s.sobolewski@hotmail.com>
2024-06-13 12:17:46 +0000

    update methods

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Connecting the necessary modules
+// Import necessary modules
 const express = require("express");
 const multer = require("multer");
 const fs = require('fs');
@@ -6,12 +6,18 @@ const path = require('path');
 const hbs = require("hbs");
 
 const app = express();
-app.use(express.static('public')); // Connecting static files (CSS, JS, images)
 
-// Path to the JSON-file with data  about downloaded files
+// Serve static files (CSS, JS, images) from the 'public' directory
+app.use(express.static('public'));
+
+// Enable parsing of request bodies (form data and JSON)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Define the path to the file where uploaded files data is stored
 const DATA_FILE = path.join(__dirname, 'filesData.json');
 
-// Function for loading data from a file
+// Function to load data from the JSON file
 function loadFileData() {
     try {
         return JSON.parse(fs.readFileSync(DATA_FILE, 'utf8'));
@@ -21,47 +27,67 @@ function loadFileData() {
     }
 }
 
-// Function of saving data to file
+// Function to save data to the JSON file
 function saveFileData(data) {
     fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2), 'utf8');
 }
 
-// Setting up multer to save downloaded files
+// Configure multer for file upload handling
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, 'cloud')); // Directory for saving files
+        cb(null, path.join(__dirname, 'cloud')); // Set directory to save files
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname); // Saving files under original names
+        cb(null, file.originalname); // Use the original file name
     }
 });
 
 const upload = multer({ storage: storage });
 
+// Set view engine to Handlebars and register partial templates
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partial");
 
-// Registering the 'times' helper to create loops in Handlebars templates
+// Register a 'times' helper for Handlebars to create loops
 hbs.registerHelper('times', function(n, block) {
     var accum = '';
-    for(var i = 0; i < n; ++i) {
+    for(var i = 0; i < n; ++i)
         accum += block.fn(i);
-    }
     return accum;
 });
 
-// Content Security Policy
+// Create directory for JSON files of new themes if it does not exist
+const dataJsonsPath = path.join(__dirname, 'datajsons');
+if (!fs.existsSync(dataJsonsPath)) {
+    fs.mkdirSync(dataJsonsPath, { recursive: true });
+}
+
+// Function to read the list of themes from the directory
+function getThemesList() {
+    const themesDir = path.join(__dirname, 'datajsons');
+    try {
+        return fs.readdirSync(themesDir).map(file => ({
+            name: file.split('.')[0],
+            path: path.join(themesDir, file)
+        }));
+    } catch (error) {
+        console.error("Failed to read themes directory:", error);
+        return [];
+    }
+}
+
+// Apply Content Security Policy (CSP)
 app.use((req, res, next) => {
     res.setHeader("Content-Security-Policy", 
         "default-src 'self'; " +
-        "script-src 'self' https://cdn.jsdelivr.net; " +  // Allowing scripts from CDN
-        "style-src 'self' https://cdn.jsdelivr.net 'unsafe-inline'; " +  // Resolving styles from CDN and inline styles
-        "font-src 'self' https://cdn.jsdelivr.net; " +  // Resolving fonts from CDN
-        "img-src 'self';"); // Resolution of images from current source
+        "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; " +
+        "style-src 'self' https://cdn.jsdelivr.net 'unsafe-inline'; " +
+        "font-src 'self' https://cdn.jsdelivr.net; " +
+        "img-src 'self';");
     next();
 });
 
-// Home page route
+// Routes
 app.get("/", function (req, res) {
     res.render("index", {
         title: "Main page - Mini Cloud",
@@ -69,52 +95,60 @@ app.get("/", function (req, res) {
     });
 });
 
-// Docs page route
-app.get("/docs", function(request,response){
-    response.render("docs",{
+app.get("/docs", function(request, response){
+    response.render("docs", {
         title: "Mini Cloud Documentation",
-        description: "Description:  ",
+        description: "Learn how to use Mini Cloud effectively.",
     });
 });
 
-// File download page route
+app.get('/config', function(req, res) {
+    const themesList = getThemesList();
+    res.render('config', {
+        title: 'Configure Themes',
+        themes: themesList
+    });
+});
+
+// Handle theme creation
+app.post('/config', function(req, res) {
+    const themeName = req.body.themeName;
+    const themeType = req.body.themeType;
+    const filePath = path.join(dataJsonsPath, `${themeName}-${themeType}.json`);
+
+    try {
+        if (!fs.existsSync(filePath)) {
+            fs.writeFileSync(filePath, JSON.stringify([]));
+            res.json({ success: true, message: 'Theme created successfully!' });
+        } else {
+            res.json({ success: false, message: 'Theme already exists.' });
+        }
+    } catch (error) {
+        console.error("Error handling request:", error);
+        res.status(500).send('Server error');
+    }
+});
+
+// Handle theme deletion
+app.post('/delete-theme/:themeName', function(req, res) {
+    const themeName = req.params.themeName;
+    const filePath = path.join(dataJsonsPath, `${themeName}.json`);
+
+    try {
+        fs.unlinkSync(filePath);
+        res.json({ success: true });
+    } catch (error) {
+        console.error("Failed to delete theme file:", error);
+        res.status(500).json({ success: false, message: "Error deleting file", error: error.message });
+    }
+});
+
 app.get("/upload", function (req, res) {
     res.render("upload", {
         title: "Upload Files"
     });
 });
 
-// File download route
-app.get('/download/:index', function (req, res) {
-    const index = parseInt(req.params.index, 10);
-    const filesData = loadFileData();
-
-    if (index >= 0 && index < filesData.length) {
-        const file = filesData[index];
-        res.download(path.join(__dirname, 'cloud', file.filename), file.filename, (err) => {
-            if (err) {
-                return res.status(500).send("Could not download the file.");
-            }
-        });
-    } else {
-        res.status(404).send('File not found.');
-    }
-});
-
-// Route to open a file in a browser
-app.get('/open/:filename', function (req, res) {
-    const filename = req.params.filename;
-    const filePath = path.join(__dirname, 'cloud', filename);
-
-    fs.access(filePath, fs.constants.F_OK, (err) => {
-        if (err) {
-            return res.status(404).send('File not found.');
-        }
-        res.sendFile(filePath);
-    });
-});
-
-// File download route
 app.post("/upload", upload.single('filedata'), function (req, res) {
     const filedata = req.file;
     if (!filedata) {
@@ -123,57 +157,29 @@ app.post("/upload", upload.single('filedata'), function (req, res) {
             message: 'No file selected.',
             messageClass: 'alert-danger'
         });
-        return;
-    }
-
-    const filesData = loadFileData();
-    const fileExists = filesData.some(f => f.filename === filedata.originalname);
-
-    if (fileExists) {
-        res.render('upload', {
-            title: 'Upload files:',
-            message: `A file named "${filedata.originalname}" already exists. Please rename your file and try again.`,
-            messageClass: 'alert-danger'
-        });
     } else {
-        filesData.push({
-            filename: filedata.originalname,
-            size: filedata.size,
-            time: new Date().toLocaleTimeString(),
-            date: new Date().toLocaleDateString()
-        });
-        saveFileData(filesData);
-        res.render('upload', {
-            title: 'Upload files:',
-            message: 'File uploaded successfully!',
-            messageClass: 'alert-success'
-        });
-    }
-});
-
-// Route to delete the file
-app.post('/delete/:index', function(req, res) {
-    const index = parseInt(req.params.index, 10);
-    let filesData = loadFileData();
-
-    if (index >= 0 && index < filesData.length) {
-        const filePath = path.join(__dirname, 'cloud', filesData[index].filename);
-        fs.unlink(filePath, (err) => {
-            if (err) {
-                console.error('File deletion error:', err);
-                res.status(500).send('Error deleting the file.');
-                return;
-            }
-
-            // Remove the entry from the data file after deleting the file
-            filesData.splice(index, 1);
+        const filesData = loadFileData();
+        const fileExists = filesData.some(f => f.filename === filedata.originalname);
+        if (fileExists) {
+            res.render('upload', {
+                title: 'Upload files:',
+                message: `A file named "${filedata.originalname}" already exists. Please rename your file and try again.`,
+                messageClass: 'alert-danger'
+            });
+        } else {
+            filesData.push({
+                filename: filedata.originalname,
+                size: filedata.size,
+                time: new Date().toLocaleTimeString(),
+                date: new Date().toLocaleDateString()
+            });
             saveFileData(filesData);
-
-            // Redirect the user back to the main page
-            res.redirect('/');
-        });
-    } else {
-        res.status(404).send('File not found.');
+            res.render('upload', {
+                title: 'Upload files:',
+                message: 'File uploaded successfully!',
+                messageClass: 'alert-success'
+            });
+        }
     }
 });
 
