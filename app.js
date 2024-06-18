961f531d9584f1d3c1d245699c158d07464695b8
Siergej Sobolewski <s.sobolewski@hotmail.com>
2024-06-18 22:25:47 +0000

    update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const multer = require("multer");
 const fs = require('fs');
 const path = require('path');
 const hbs = require("hbs");
@@ -19,24 +18,6 @@ app.use(express.static('public', {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set up the view engine and register partial templates
-app.set("view engine", "hbs");
-hbs.registerPartials(__dirname + "/views/partial");
-
-// Register 'times' helper for creating loops in Handlebars templates
-hbs.registerHelper('times', function(n, block) {
-    var accum = '';
-    for(var i = 0; i < n; ++i)
-        accum += block.fn(i);
-    return accum;
-});
-
-// Create a directory for JSON files of new themes, if it does not exist
-const dataJsonsPath = path.join(__dirname, 'datajsons');
-if (!fs.existsSync(dataJsonsPath)) {
-    fs.mkdirSync(dataJsonsPath, { recursive: true });
-}
-
 // Content Security Policy (CSP)
 app.use((req, res, next) => {
     res.setHeader("Content-Security-Policy",
@@ -48,16 +29,31 @@ app.use((req, res, next) => {
     next();
 });
 
-// Routes
+// Set up the view engine and register partial templates
+app.set("view engine", "hbs");
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, "views/partial"));
+hbs.registerHelper('times', function(n, block) {
+    let accum = '';
+    for(let i = 0; i < n; ++i)
+        accum += block.fn(i);
+    return accum;
+});
+
+// Web Routes
 const storageRoutes = require('./routes/storage');
 const uploadRoutes = require('./routes/upload');
 const configRoutes = require('./routes/config');
 const docsRoutes = require('./routes/docs');
 
+// API Routes
+const apiRoutes = require('./routes/api');
+
 app.use('/', storageRoutes);
 app.use('/', uploadRoutes);
 app.use('/', configRoutes);
 app.use('/', docsRoutes);
+app.use('/api', apiRoutes);
 
 // Server start
 app.listen(3000, function () {
