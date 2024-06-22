54b521c7a236f526f4e1acfeeb433e8e670ec994
Siergej Sobolewski <s.sobolewski@hotmail.com>
2024-06-22 23:43:02 +0000

    update

--- app.js
+++ app.js
@@ -1,46 +1,34 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const hbs = require("hbs");
-
 const app = express();
+const hbs = require('hbs');
 
 // Serve static files (CSS, JS, images)
-app.use(express.static('public', {
+app.use(express.static(path.join(__dirname, 'public'), {
     setHeaders: function (res, path) {
-        if (path.endsWith(".js")) {
+        if (path.endsWith('.js')) {
             res.set('Content-Type', 'application/javascript');
+        }
+        if (path.endsWith('.css')) {
+            res.set('Content-Type', 'text/css');
         }
     }
 }));
 
-// Enable parsing of request bodies in urlencoded and JSON formats
+// Middleware to parse incoming requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Content Security Policy (CSP)
-app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy",
-        "default-src 'self'; " +
-        "img-src 'self' data:; " +
-        "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; " +
-        "style-src 'self' https://cdn.jsdelivr.net 'unsafe-inline'; " +
-        "font-src 'self' https://cdn.jsdelivr.net;");
-    next();
-});
-
 // Set up the view engine and register partial templates
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-hbs.registerPartials(path.join(__dirname, "views/partial"));
+hbs.registerPartials(path.join(__dirname, 'views/partial'));
 
 // Web Routes
 const storageRoutes = require('./routes/storage');
 const uploadRoutes = require('./routes/upload');
 const configRoutes = require('./routes/config');
 const docsRoutes = require('./routes/docs');
-
-// API Routes
 const apiRoutes = require('./routes/api');
 
 app.use('/', storageRoutes);
